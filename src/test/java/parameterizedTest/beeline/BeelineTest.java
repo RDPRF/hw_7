d14acebd5719a45c190e1fb75aae03e030ca3dea
Dmitry Rodichev <ya.vilnes@yandex.ru>
2022-12-14 21:46:34 +0700

    Added test TelecomArmenia, adds test case

--- src/test/java/parameterizedTest/beeline/BeelineTest.java
+++ src/test/java/parameterizedTest/beeline/BeelineTest.java
@@ -1,19 +1,27 @@
-package parameterizedTest;
+package parameterizedTest.beeline;
 
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import parameterizedTest.TestBase;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class BeelineTest extends TestBase {
 
     @BeforeEach
     void setUp() {
-        open("https://moskva.beeline.ru/customers/products/");
+        open("https://moskva.beeline.ru/");
     }
 
     @ValueSource(strings = {"Частным лицам", "Поставщикам и партнёрам", "Офисы и покрытие", "Помощь и поддержка",})
@@ -34,7 +42,4 @@ public class BeelineTest extends TestBase {
         $("#searchForm").setValue(item);
         $(".swiper-wrapper").shouldHave(text(foundItem));
     }
-
 }
-
-
